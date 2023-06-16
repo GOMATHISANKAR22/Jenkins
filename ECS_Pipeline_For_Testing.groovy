96dff3edf4624b77f26ce6cd977055b0b63cfab9
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-06-16 13:26:14 +0530

    Update ECS_Pipeline_For_Testing.groovy

--- ECS_Pipeline_For_Testing.groovy
+++ ECS_Pipeline_For_Testing.groovy
@@ -5,8 +5,8 @@ pipeline {
         string(name: 'Jenkins_IP',description: 'Enter the Jenkins IP')
         string(name: 'ECR_Repo_Name', defaultValue: 'test',description: 'Enter the ECR Repositary Name') 
         string(name: 'Version_Number', defaultValue: '1.0', description: 'Enter the Version Number for ECR Image')
-        choice  (choices: ["us-east-1", "us-east-2"],
-                 description: 'Enter your Region Name (eg: us-east-1)',
+        choice  (choices: ["us-east-1", "us-east-2",],
+                 description: 'Select your Region Name (eg: us-east-1). To Know your region code refer https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html#Concepts.RegionsAndAvailabilityZones.Regions ',
                  name: 'Region_Name')      
         
         string(name: 'Aws_Id' ,description: 'Enter the AWS Id')
