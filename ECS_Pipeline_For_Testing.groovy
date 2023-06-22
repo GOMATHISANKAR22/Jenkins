643d1423ae01480b988322e4267e025c9f39a011
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-06-22 07:26:23 +0100

    Update ECS_Pipeline_For_Testing.groovy

--- ECS_Pipeline_For_Testing.groovy
+++ ECS_Pipeline_For_Testing.groovy
@@ -135,7 +135,7 @@ pipeline {
                     if (stackExists == 0) {
                         script {
                             sh '''
-                            aws cloudformation update-stack --stack-name ${Stack_Name} --template-url ${S3_Url} --capabilities CAPABILITY_NAMED_IAM --parameters file:/existing-parameters.json ParameterKey=ImageId,ParameterValue=${AWS_Account_Id}.dkr.ecr.${Region_Name}.amazonaws.com/${ECR_Repo_Name}:${Version_Number} ParameterKey=ContainerPort,ParameterValue=${Container_Port}   
+                            aws cloudformation update-stack --stack-name ${Stack_Name} --template-url ${S3_Url} --capabilities CAPABILITY_NAMED_IAM --parameter-overrides file:/existing-parameters.json ParameterKey=ImageId,ParameterValue=${AWS_Account_Id}.dkr.ecr.${Region_Name}.amazonaws.com/${ECR_Repo_Name}:${Version_Number} ParameterKey=ContainerPort,ParameterValue=${Container_Port}   
                             
                            '''
                         }
